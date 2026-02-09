c1b47eafbbf945e3813a7bb0bace5ac9375cbd23
Ben Fereydouni <bfereydo@gmail.com>
2026-02-09 13:43:24 -0800

    Implement HTN operators, methods, and heuristic

--- src/autoHTN.py
+++ src/autoHTN.py
@@ -17,9 +17,20 @@ pyhop.declare_methods('produce', produce)
 
 def make_method(name, rule):
 	def method(state, ID):
-		# your code here
-		pass
+		subtasks = []
+		requires = rule.get('Requires', {})
+		consumes = rule.get('Consumes', {})
 
+		for item, num in requires.items():
+			subtasks.append(('have_enough', ID, item, num))
+
+		for item, num in consumes.items():
+			subtasks.append(('have_enough', ID, item, num))
+
+		subtasks.append(('op_{}'.format(name), ID))
+		return subtasks
+
+	method.__name__ = 'produce_{}'.format(name)
 	return method
 
 def declare_methods(data):
@@ -27,26 +38,63 @@ def declare_methods(data):
 	# sort the recipes so that faster recipes go first
 
 	# your code here
-	# hint: call make_method, then declare the method to pyhop using pyhop.declare_methods('foo', m1, m2, ..., mk)	
-	pass			
+	# hint: call make_method, then declare the method to pyhop using pyhop.declare_methods('foo', m1, m2, ..., mk)
+	method_map = {}
+	for recipe_name, rule in data['Recipes'].items():
+		product = next(iter(rule['Produces']))
+		method = make_method(_normalize_name(recipe_name), rule)
+		method_map.setdefault(product, []).append((rule['Time'], method, recipe_name))
+
+	for product, methods in method_map.items():
+		methods.sort(key=lambda item: item[0])
+		ordered_methods = [method for _, method, _ in methods]
+		pyhop.declare_methods('produce_{}'.format(product), *ordered_methods)
 
 def make_operator(rule):
 	def operator(state, ID):
-		# your code here
-		pass
+		if state.time[ID] < rule['Time']:
+			return False
+
+		requires = rule.get('Requires', {})
+		for item, num in requires.items():
+			if getattr(state, item)[ID] < num:
+				return False
+
+		consumes = rule.get('Consumes', {})
+		for item, num in consumes.items():
+			if getattr(state, item)[ID] < num:
+				return False
+
+		for item, num in consumes.items():
+			getattr(state, item)[ID] -= num
+
+		for item, num in rule.get('Produces', {}).items():
+			getattr(state, item)[ID] += num
+
+		state.time[ID] -= rule['Time']
+		return state
 	return operator
 
 def declare_operators(data):
 	# your code here
 	# hint: call make_operator, then declare the operator to pyhop using pyhop.declare_operators(o1, o2, ..., ok)
-	pass
+	operators = []
+	for recipe_name, rule in data['Recipes'].items():
+		operator = make_operator(rule)
+		operator.__name__ = 'op_{}'.format(_normalize_name(recipe_name))
+		operators.append(operator)
+
+	pyhop.declare_operators(*operators)
 
 def add_heuristic(data, ID):
 	# prune search branch if heuristic() returns True
 	# do not change parameters to heuristic(), but can add more heuristic functions with the same parameters: 
 	# e.g. def heuristic2(...); pyhop.add_check(heuristic2)
 	def heuristic(state, curr_task, tasks, plan, depth, calling_stack):
-		# your code here
+		if curr_task[0].startswith('produce_'):
+			for task in calling_stack:
+				if task[0] == curr_task[0]:
+					return True
 		return False # if True, prune this branch
 
 	pyhop.add_check(heuristic)
@@ -58,6 +106,9 @@ def define_ordering(data, ID):
 		return methods
 	
 	pyhop.define_ordering(reorder_methods)
+
+def _normalize_name(name):
+	return name.replace(' ', '_')
 
 def set_up_state(data, ID):
 	state = pyhop.State('state')
